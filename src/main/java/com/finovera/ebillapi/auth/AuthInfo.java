d0ffc961fca41375f9c0b3c5260998c4937a2bdb
Lokanath Behera <lokanath@fiport.com>
2016-05-23 18:33:59 +0530

    Basic AuthInfo Class added

--- src/main/java/com/finovera/ebillapi/auth/AuthInfo.java
+++ src/main/java/com/finovera/ebillapi/auth/AuthInfo.java
@@ -1,6 +1,10 @@
 package com.finovera.ebillapi.auth;
 
+import java.util.Arrays;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 public class AuthInfo {
@@ -11,10 +15,40 @@ public class AuthInfo {
 
 	String apiVer = "1.0";
 
-	public void init(final RestTemplate template, final HttpHeaders headers) {
-		this.template = template;
-		this.headers = headers;
-		headers.add("X-FINOVERA-VERSION", apiVer);
+	String url;
+
+	public void login(final MultiValueMap<String, String> credMap) {
+		try {
+
+			final CustomerLoginData loginData = template.postForObject(url + "/login/ps", credMap, CustomerLoginData.class);
+			if (loginData.status.equals("SUCCESS")) {
+				final HttpHeaders headers = new HttpHeaders();
+				headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				headers.add("X-FINOVERA-TOKEN", loginData.token);
+
+			} else {
+
+			}
+
+		} catch (final Exception e) {
+
+		}
+	}
+
+	public static class CustomerLoginData {
+
+		public String status;
+		public String token;
+		public long vaidUntil;
+	}
+
+	static class UserLoginData {
+		public String token;
+		public String userId;
+		public String name;
+		public String zip;
+		public String email;
 	}
 
 }
