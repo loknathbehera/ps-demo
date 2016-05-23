34dffbbc1aceebec58a08151682555f8bf10aa75
Lokanath Behera <lokanath@fiport.com>
2016-05-23 18:44:25 +0530

    AuthInfo modified

--- src/main/java/com/finovera/ebillapi/App.java
+++ src/main/java/com/finovera/ebillapi/App.java
@@ -1,10 +1,17 @@
 package com.finovera.ebillapi;
 
-import com.finovera.ebillApi.ui.AddBillerFrame;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import com.finovera.ebillapi.auth.AuthInfo;
 
 public class App {
 	public static void main(final String[] args) {
-		final AddBillerFrame addBiller = new AddBillerFrame();
-		addBiller.addBillerPage();
+		final AuthInfo auth = new AuthInfo("http://localhost:8085/spring");
+		final MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+		map.add("customerCode", "");
+		map.add("password", "");
+
+		auth.login(map);
 	}
 }
--- src/main/java/com/finovera/ebillapi/auth/AuthInfo.java
+++ src/main/java/com/finovera/ebillapi/auth/AuthInfo.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import com.finovera.ebillApi.ui.AddBillerFrame;
+
 public class AuthInfo {
 
 	RestTemplate template;
@@ -17,16 +19,24 @@ public class AuthInfo {
 
 	String url;
 
+	public AuthInfo(final String url) {
+		template = new RestTemplate();
+		this.url = url;
+	}
+
 	public void login(final MultiValueMap<String, String> credMap) {
 		try {
 
 			final CustomerLoginData loginData = template.postForObject(url + "/login/ps", credMap, CustomerLoginData.class);
 			if (loginData.status.equals("SUCCESS")) {
-				final HttpHeaders headers = new HttpHeaders();
+
+				headers = new HttpHeaders();
 				headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 				headers.setContentType(MediaType.APPLICATION_JSON);
 				headers.add("X-FINOVERA-TOKEN", loginData.token);
 
+				final AddBillerFrame addBiller = new AddBillerFrame();
+				addBiller.addBillerPage();
 			} else {
 
 			}
