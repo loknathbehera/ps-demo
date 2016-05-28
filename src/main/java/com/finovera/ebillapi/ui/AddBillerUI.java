abc9f70334c8473855cae3aff5b190e32678625d
loknath <loknath@DESKTOP-H4LJJJ7>
2016-05-28 21:32:40 +0530

    AddBiller UI implemented

--- src/main/java/com/finovera/ebillapi/ui/AddBillerUI.java
+++ src/main/java/com/finovera/ebillapi/ui/AddBillerUI.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 import com.finovera.ebillapi.auth.AuthInfo;
@@ -21,11 +22,12 @@ public class AddBillerUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField immuitableIdtextField;
-	private JTextField credTextField;
+	JPanel credsPanel;
 
 	AuthInfo authInfo;
 
 	AddBillerServices addBillerService;
+	private JTextField credTextField;
 
 	/**
 	 * Create the frame.
@@ -48,54 +50,65 @@ public class AddBillerUI extends JFrame {
 				addBillerService = new AddBillerServices(authinfo);
 				Biller biller = addBillerService.getBillerInfo(e.getActionCommand());
 				createCredPanel(biller);
+
+				credsPanel.setVisible(true);
 			}
 		});
 		immuitableIdtextField.setToolTipText("Please Enter the Immuitable Id of the biller which you need to add");
 		immuitableIdtextField.setHorizontalAlignment(SwingConstants.LEFT);
 		immuitableIdtextField.setColumns(10);
+
+		credsPanel = new JPanel();
+		credsPanel.setBackground(UIManager.getColor("Button.shadow"));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane
-				.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup().addGap(10)
-								.addComponent(immuitableIdLbl, GroupLayout.PREFERRED_SIZE, 76,
-										GroupLayout.PREFERRED_SIZE)
-								.addGap(43)
-								.addComponent(immuitableIdtextField, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE)
-								.addGap(87)));
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup().addGap(10)
+										.addComponent(immuitableIdLbl, GroupLayout.PREFERRED_SIZE, 76,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(18).addComponent(immuitableIdtextField, GroupLayout.PREFERRED_SIZE, 0,
+												Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(30).addComponent(credsPanel,
+								GroupLayout.DEFAULT_SIZE, 365, Short.MAX_VALUE)))
+						.addGap(29)));
 		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addGap(18).addComponent(immuitableIdLbl))
-				.addComponent(immuitableIdtextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-						GroupLayout.PREFERRED_SIZE));
+				.addGroup(gl_contentPane.createSequentialGroup().addGap(12)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addComponent(immuitableIdLbl)
+								.addComponent(immuitableIdtextField, GroupLayout.PREFERRED_SIZE,
+										GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGap(32).addComponent(credsPanel, GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
+						.addGap(22)));
+		credsPanel.setLayout(null);
+		credsPanel.setVisible(false);
+
 		contentPane.setLayout(gl_contentPane);
 
 	}
 
-	protected void createCredPanel(Biller biller2) {
+	protected void createCredPanel(Biller biller) {
 
-		JPanel credPanel = new JPanel();
-		// sl_contentPane.putConstraint(SpringLayout.NORTH, credPanel, 46,
-		// SpringLayout.NORTH, contentPane);
-		// sl_contentPane.putConstraint(SpringLayout.SOUTH, credPanel, -1,
-		// SpringLayout.SOUTH, contentPane);
-		// sl_contentPane.putConstraint(SpringLayout.SOUTH,
-		// immuitableIdtextField, -6, SpringLayout.NORTH, credPanel);
-		// sl_contentPane.putConstraint(SpringLayout.WEST, credPanel, 97,
-		// SpringLayout.WEST, contentPane);
-		// sl_contentPane.putConstraint(SpringLayout.EAST, credPanel, 4,
-		// SpringLayout.EAST, contentPane);
-		// contentPane.add(credPanel);
-		credPanel.setLayout(null);
+		for (CredentialFieldInfo credentialFieldInfo : biller.getCredentialFieldInfoList()) {
 
-		CredentialFieldInfo[] credentialFieldInfo = biller2.getCredentialFieldInfoList();
+			JPanel credPanel = new JPanel();
+			credPanel.setBounds(38, 11, 306, 36);
+			credsPanel.add(credPanel);
+			credPanel.setLayout(null);
 
-		JLabel credDefNameLbl = new JLabel("");
-		credDefNameLbl.setBounds(10, 52, 46, 14);
-		credPanel.add(credDefNameLbl);
+			JLabel credlbl = new JLabel(credentialFieldInfo.getName());
+			credlbl.setText(credentialFieldInfo.getName());
+			credlbl.setBounds(10, 11, 61, 14);
+			credlbl.setHorizontalAlignment(SwingConstants.LEFT);
+			credPanel.add(credlbl);
 
-		credTextField = new JTextField();
-		credTextField.setBounds(100, 49, 157, 20);
-		credPanel.add(credTextField);
-		credTextField.setColumns(10);
+			credTextField = new JTextField();
+			credTextField.setName(credentialFieldInfo.getName() + "_txtFiled");
+			credTextField.setBounds(81, 8, 215, 20);
+			credPanel.add(credTextField);
+			credTextField.setColumns(10);
 
+			credsPanel.add(credPanel);
+
+		}
 	}
 }
